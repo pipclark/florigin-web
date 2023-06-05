b9d718c726e974bfda1e09e26b5dd2e8f1de5ff2
Work Application <workapplication@Works-MBP.fritz.box>
2023-06-05 13:28:48 +0200

    add contact info

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Contact() {
-	return <div>Coming soon...</div>;
+	return (
+		<div>
+			<h2>Contact Us</h2>
+			<h3>floriginmusic@gmail.com</h3>
+			<p>We're open to playing concerts and festivals in Europe</p>
+		</div>
+	);
 }
