02747686d9976ab8ec21d2386ff66beb0aa75b18
Work Application <workapplication@Works-MBP.fritz.box>
2023-06-06 18:11:50 +0200

    show previous gigs, before todays date

--- src/components/GigList.tsx
+++ src/components/GigList.tsx
@@ -24,6 +24,35 @@ export default function GigList({ gigs }: GigListProps) {
 			<h2>Upcoming Gigs</h2>
 			<ul className="gigList">
 				{gigs
+					.filter((gig) => Date.parse(gig.dateAndTime) > Date.now())
+					.sort((a, b) => {
+						return Date.parse(a?.dateAndTime) - Date.parse(b?.dateAndTime);
+					})
+					.map((gig) => (
+						<li className="gigItem" key={gig?.title} onClick={() => gig?.link}>
+							<ul className="gigDetails">
+								<li className="dateTime">
+									{
+										// TODO: add tyoes for toLocaleDateString arguments
+										new Date(gig?.dateAndTime).toLocaleDateString(
+											"en-GB",
+											//@ts-ignore
+											options
+										)
+									}
+								</li>
+								<li className="cityCountry">{gig.citycountry}</li>
+								<li className="venue">
+									<Link to={gig?.link}> {gig?.title}</Link>
+								</li>
+							</ul>
+						</li>
+					))}
+			</ul>
+			<h3>Previous Gigs</h3>
+			<ul className="gigList">
+				{gigs
+					.filter((gig) => Date.parse(gig.dateAndTime) < Date.now())
 					.sort((a, b) => {
 						return Date.parse(a?.dateAndTime) - Date.parse(b?.dateAndTime);
 					})
