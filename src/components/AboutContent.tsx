cf41fb70034e4a2473132dc02583871d60755799
Work Application <workapplication@Works-MBP.fritz.box>
2023-06-06 18:00:29 +0200

    add about us from content manager
    parses HTML but style does not go through

---
+++ src/components/AboutContent.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { getContentful } from "../lib/getContentful";
+import ReactHtmlParser from "react-html-parser";
+
+type BandInfo = {
+	bandName: string;
+	aboutText: string;
+	photo: {
+		title: string;
+		description: string;
+		url: string;
+		width: number;
+		height: number;
+	};
+};
+
+type AboutProps = {
+	contentfulUrl: string;
+};
+
+export default function AboutContent(props: AboutProps) {
+	const [info, setInfo] = useState<BandInfo | undefined>();
+	const [parsedAboutText, setParsedAboutText] = useState<
+		React.ReactNode | undefined
+	>();
+	const contentKey = "bandInfo";
+	const contentId = "4nWy68ynMWkH9dzrmtoWBL";
+	const contentfulUrl = props.contentfulUrl;
+
+	const query = `
+  {
+    ${contentKey}(id: "${contentId}") {
+				aboutText 
+				photo {
+					url
+					title
+				}
+			}
+    }
+    `;
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await getContentful<Record<string, BandInfo>>(
+				contentfulUrl,
+				query
+			);
+			setInfo(data?.[contentKey]);
+		};
+		fetchData();
+	}, [contentfulUrl, query]);
+
+	useEffect(() => {
+		if (info) {
+			console.log(info.aboutText);
+			setParsedAboutText(ReactHtmlParser(info?.aboutText));
+		}
+	}, [info]);
+
+	if (!info) {
+		return <p>Loading...</p>;
+	} else {
+		return <div>{parsedAboutText}</div>;
+	}
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/About.css";
+import { getContentful } from "../lib/getContentful";
+import AboutContent from "../components/AboutContent";
+import { contentfulUrl } from "../urls";
 
 export default function About() {
-	return <div>Coming soon...</div>;
+	return (
+		<div className="about">
+			<h2>About Us</h2>
+			<AboutContent contentfulUrl={contentfulUrl}></AboutContent>
+		</div>
+	);
 }
