72de9c41452b517832e30d8b37bad8e20eeda34c
Work Application <workapplication@Works-MBP.fritz.box>
2023-06-27 18:26:42 +0200

    add random photo to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,14 @@ import "../styles/About.css";
 import { getContentful } from "../lib/getContentful";
 import AboutContent from "../components/AboutContent";
 import { contentfulUrl } from "../urls";
+import RandomPhoto from "../components/RandomPhoto";
 
 export default function About() {
 	return (
 		<div className="about">
 			<h2>About Us</h2>
 			<AboutContent contentfulUrl={contentfulUrl}></AboutContent>
+			<RandomPhoto contentfulUrl={contentfulUrl}></RandomPhoto>
 		</div>
 	);
 }
